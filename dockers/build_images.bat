917ccf17c8b01bd5f7aef092d1748b511c4de432
guaotiantang <guaotiantang@outlook.com>
2025-05-02 07:33:06 +0800

    Initial project setup with VSCode config, Node.js packages and Docker build scripts

--- dockers/build_images.bat
+++ dockers/build_images.bat
@@ -1,6 +1,15 @@
-del m-python.tar
-del m-nodejs.tar
+del .\nodejs\prisma\schema.prisma
+del .\nodejs\package.json
+del .\nodejs\yarn.lock
+copy ..\Center\prisma\schema.prisma .\nodejs\prisma\schema.prisma
+copy ..\Center\package.json .\nodejs\package.json
+copy ..\Center\yarn.lock .\nodejs\yarn.lock
+
+del ..\dist\m-python.tar
+del ..\dist\m-nodejs.tar
 docker build -t m-python:latest .\python
 docker build -t m-nodejs:latest .\nodejs
-docker save -o m-python.tar m-python:latest
-docker save -o m-nodejs.tar m-nodejs:latest
+docker save -o ..\dist\m-python.tar m-python:latest
+docker save -o ..\dist\m-nodejs.tar m-nodejs:latest
+cd ..\Center
+yarn none
