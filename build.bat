3dbe3b4e95f7b95309df6b5edf143490a62e29ce
guaotiantang <guaotiantang@outlook.com>
2025-05-02 07:12:20 +0800

    Add package.json for Center module and optimize build script order

--- build.bat
+++ build.bat
@@ -1,23 +1,19 @@
 @echo off
-rem 删除dist目录
 rmdir /s /q dist
-cd Center
-yarn build
-cd ..
 mkdir dist
 mkdir dist\Center
 mkdir dist\Gateway
 mkdir dist\Scanner
 mkdir dist\Parser
-xcopy /E /I /Y Center\dist .\\dist\\Center\\
 xcopy /E /I /Y Gateway\dist .\\dist\\Gateway\\
 xcopy /E /I /Y Scanner\dist .\\dist\\Scanner\\
 xcopy /E /I /Y Parser\dist .\\dist\\Parser\\
 cd dist
-rem 删除dist所有子目录下的python的__pycache__目录
 for /r %%i in (__pycache__) do rmdir /s /q "%%i"
 cd ..
-
+cd Center
+yarn build
+cd ..
 
 
 
